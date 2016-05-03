d4c83a68c53f14647b20088c56c732d213c05b7d
Bill Abt <babt@me.com>
2016-05-03 14:33:45 -0400

    Added dependencies for OpenSSL.

--- Package.swift
+++ Package.swift
@@ -13,3 +13,13 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 **/
+
+import PackageDescription
+
+let package = Package(
+	name: "CCryptoSSL",
+	pkgConfig: "openssl",
+	providers: [
+		.Apt("openssl libssl-dev"), 
+	]
+)
