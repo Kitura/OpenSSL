aafc154e50107ab62d710a81511198aaa331aabb
giginet <giginet.net@gmail.com>
2018-07-11 05:20:09 +0900

    Add brew provider

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -22,7 +22,8 @@ import PackageDescription
 let package = Package(
     name: "OpenSSL",
     providers: [
-        .apt(["openssl libssl-dev"])
+        .apt(["openssl libssl-dev"]),
+        .brew(["openssl"]),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
